83432aaae0d09f9665308982d1af55287f5f2123
thfreud <thfreud@gmail.com>
2025-08-07 23:03:08 -0300

    Segunda atualização

--- test.jl
+++ test.jl
@@ -1,1 +1,15 @@
-println("Hello Test")
+mutable struct Pessoa
+    idade
+    nome
+end
+
+
+mutable struct Engenheiro
+    pessoa::Pessoa
+    salario
+end
+
+pessoa = Pessoa(40,"Paulo")
+
+pessoa_engenheira = Engenheiro(pessoa, 1500)
+println(pessoa_engenheira.pessoa.nome)

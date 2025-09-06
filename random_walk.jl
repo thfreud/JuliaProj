f9ae0b997f9c01314b4634442b135ce05c105e0b
thfreud <thfreud@gmail.com>
2025-09-05 22:02:12 -0300

    some adjust

--- random_walk.jl
+++ random_walk.jl
@@ -2,45 +2,60 @@
 
 using Random
 using Plots
-max_distance::Int = 50000
-path_x = zeros(Int,max_distance)
-path_y = zeros(Int, max_distance)
 
+# define a type Walker and a constructor for Walker
 mutable struct Walker
     x::Int
-    y::Int    
+    y::Int
+    path_x::Vector{Int}
+    path_y::Vector{Int}
+end
+# constructor for Walker
+function Walker(x::Int, y::Int, max_distance::Int)
+    Walker(x, y, zeros(Int, max_distance), zeros(Int, max_distance))
 end
 
-function move!(walker::Walker, direction)
-step = rand([-1,1])
-    if direction==0
+function move!(walker::Walker, step_index::Int)
+    direction = rand([0, 1])
+    step = rand([-1, 1])
+    if direction == 0
         walker.x += step
     else
         walker.y += step
     end
+
+    walker.path_x[step_index] = walker.x
+    walker.path_y[step_index] = walker.y
 end
 
-function path!(walker::Walker, path_x, path_y, max_distance)
-    path_x[1] = walker.x
-    path_y[1] = walker.y
-    for i in 2:max_distance
-        move!(walker, rand(0:1))
-        path_x[i] = walker.x
-        path_y[i] = walker.y
+function random_walk(x::Int, y::Int, max_distance)
+    #declaring walker
+    walker = Walker(x, y, max_distance)
+    for step_index in 2:max_distance
+        move!(walker, step_index)
     end
+    return walker
 end
 
-walker = Walker(0,0)
-path!(walker, path_x, path_y, max_distance)
+@time begin
+    # The number of steps for the random walk.
+    num_steps = 10000
 
-p = plot(
-    path_x, 
-    path_y, 
-    title="Simulação de Passeio Aleatório", 
-    xlabel="Posição X", 
-    ylabel="Posição Y", 
-    label="Caminho", 
-    legend=:topright,
-    size=(800, 800)
-)
-savefig(p,"plot.png")
+    # Perform the random walk.
+    final_walker = random_walk(0, 0, num_steps)
+
+    # Create a plot using the path data stored in the Walker object.
+    p = plot(
+        final_walker.path_x,
+        final_walker.path_y,
+        title="Simulação de Passeio Aleatório",
+        xlabel="Posição X",
+        ylabel="Posição Y",
+        label="Caminho",
+        legend=:topright,
+        size=(1920, 1440)
+    )
+
+    # Save the plot to a file.
+    savefig(p, "plot.png")
+end
